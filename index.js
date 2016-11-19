048152fe6022e5bc99772338ba4ab86b0e0ed996
Rodrigo Borba <rjborba@live.com>
2016-11-19 19:23:30 -0300

    fix index 2

--- index.js
+++ index.js
@@ -45,4 +45,7 @@ app.get('/cars', function (req, res) {
     res.send(jsonFile);
 })
 
-app.listen(80);
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
