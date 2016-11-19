8b2055dfe2007ce27cd0382f2aecb4711dc79119
Rodrigo Borba <rjborba@live.com>
2016-11-19 19:59:42 -0300

    Stable!

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ app.get('/cars', function (req, res) {
     res.send(jsonFile);
 })
 
+app.get('/', function (req, res) {
+    res.send("HOME (:");
+})
+
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
